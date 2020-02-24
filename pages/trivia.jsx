69aee66c45b44c0d23fda44073289d9ad9d29664
Shirley Silvana Suarez Startary <shirley.startary@gmail.com>
2020-02-23 21:14:17 -0600

    add change (#48)
    * fix error
    
    * configuration radio button
    
    * add solucion trivia
    
    * add funcionality trivia

--- pages/trivia.jsx
+++ pages/trivia.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
+import Fab from '@material-ui/core/Fab';
+import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
+
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -12,10 +17,28 @@ import objectives from '../data/objectives';
 
 const Trivia = (props) => {
   const { objective } = props;
-  const [value, setValue] = React.useState(false);
+  const [respuestas, setRespuestas] = React.useState([
+    {
+      Pregunta1:false,
+    },{
+      Pregunta2:false,
+    },{
+      Pregunta3:false,
+    }, {
+      Pregunta4:false,
+    }, {
+      Pregunta5:false,
+    }, 
+  ]);
+  localStorage.setItem("respuestas", JSON.stringify(respuestas) );
+  
   const handleChange = (event) => {
-    setValue(event.target.value);
+    let newArr = [...respuestas]; // copying the old datas array
+    newArr[Number(event.target.name.slice(8)-1)][event.target.name] = event.target.value;
+    localStorage.setItem("respuestas", JSON.stringify(newArr) );
+    setRespuestas(newArr)
   };
+
 
   return (
     <div>
@@ -26,24 +49,38 @@ const Trivia = (props) => {
             <Typography className="title" variant="h4" gutterBottom>
               {objective.title}
             </Typography>
-            {objective.trivia.map(trivia => (
-              <Typography className="title" variant="h5" gutterBottom>
+            {objective.trivia.map((trivia, index) => {
+              
+           return (<Typography className="title" variant="h5" gutterBottom>
                 {trivia.pregunta}
-                {trivia.respuestas.map(respuesta => (
+                    {trivia.respuestas.map((respuesta) => (
                   <FormControl component="fieldset">
-                    <RadioGroup
+                    <RadioGroup 
+                    aria-label={`Pregunta${trivia.idPregunta}`} 
+                    name={`Pregunta${trivia.idPregunta}`} 
+                    value={respuestas[index][`Pregunta${trivia.idPregunta}`]} 
+                    onChange={handleChange}
+                    >
+                    {/* <RadioGroup
                       aria-label={objective.index}
                       name={objective.index}
                       value={value}
                       onChange={handleChange}
-                    >
+                    > */}
                       <FormControlLabel value={respuesta} control={<Radio />} label={respuesta} />
                     </RadioGroup>
                   </FormControl>
-                ))}
-              </Typography>
-            ))}
+                      ))}
+              </Typography>)
+          })}   
           </CardContent>
+          <Link href={`/solutions-trivia/?id=${objective.index}`} key={objective.title} prefetch>
+            <a className="objective">
+              <Fab color="secondary" aria-label="add">
+                <PlayArrowRoundedIcon />
+              </Fab>
+            </a>
+          </Link>
         </Card>
       </div>
 
