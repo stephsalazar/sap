55ce8aa38bb7ad062cb89cb247ea41533aef7fe8
AnaSalazar <anastephany.salazarguzman@gmail.com>
2020-02-27 00:07:31 -0600

    Add image for splash

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,53 +1,57 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import Link from 'next/link';
-import NavigationBar from '../components/navigationBar';
-import objectives from '../data/objectives';
+import Router from 'next/router';
+import withStyles from '@material-ui/core/styles/withStyles';
+import imageMobile from '../images/splash-mobile.png';
+import imageTablet from '../images/splash-tablet.png';
+import imageDesktop from '../images/splash-desktop.png';
 
-const App = () => (
-  <div>
-    <NavigationBar />
+const styles = (theme => ({
+  root: {
+    whidth: '100%',
+    height: '100vh',
+    [theme.breakpoints.up('xs')]:
+    {
+      backgroundImage: `url(${imageMobile})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    },
+    [theme.breakpoints.up('md')]: {
+      backgroundImage: `url(${imageTablet})`,
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+    },
+    [theme.breakpoints.up('lg')]: {
+      backgroundImage: `url(${imageDesktop})`,
+      backgroundPosition: 'center center',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed',
+      backgroundSize: 'cover',
+    },
+  },
+}));
 
-    <div className="objectives">
-      { objectives.map(objective => (
-        <Link href={`/info-objective/?id=${objective.index}`} key={objective.title} prefetch>
-          <a className="objective">
-            <img src={objective.url} alt={objective.title} />
-          </a>
-        </Link>
-      ))}
-    </div>
+class App extends React.Component {
+  componentDidMount() {
+    this.redirect = setTimeout(() => {
+      Router.push('/objectives');
+    }, 5000);
+  }
 
-    <style jsx>
-      {`
-        .objectives {
-          display: grid;
-          grid-gap: 25px;
-          padding: 15px;
-          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-          padding-top: 110px;
-        }
-        .objective {
-          display: block;
-          background: grey;
-          box-shadow: 3px 3px 3px grey;
-        }
-        .objective img {
-          border-radius: 3px;
-          width: 100%
-        }
-      `}
-    </style>
-    <style jsx global>
-      {`
-        body {
-          margin: 0;
-          font-family: system-ui;
-          background: white;
-        }
-      `}
-    </style>
-  </div>
-);
+  render() {
+    const { classes } = this.props;
 
-export default App;
+    return (
+      <div className={classes.root}>
+        <style jsx global>
+          {`
+            body {
+              margin: 0;
+            }
+          `}
+        </style>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
