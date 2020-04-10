7d9336ecef9109d6f155cf57cf35798315f19ea1
stephSalazar <anastephany.salazarguzman@gmail.com>
2020-04-10 00:24:13 -0500

    add margin in organizations

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
@@ -8,108 +9,105 @@ import imageLogoJA from '../images/splash/LogoJA.png';
 import imageSAP from '../images/splash/SAP.png';
 
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Box
-          style={{
-            backgroundImage: `url(${imageFondo})`,
-            backgroundPosition: 'center center',
-            backgroundSize: 'cover',
-            height: '100vh',
-            width: '100vw',
-          }}
-        >
-          <Box
-            style={{
-              height: '70vh',
-              width: '100%',
-            }}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <img
-              src={imageCirculo}
-              alt="fondo"
-              className="circle"
-            />
-          </Box>
-          <Box
-            style={{
-              height: '30vh',
-              width: '100%',
-            }}
-            display="flex"
-            justifyContent="center"
-            alignItems="flex-start"
-          >
-            <Link href="/objectives">
-              <a>
-                <Fab
-                  aria-label="add"
-                  variant="extended"
-                  style={{
-                  backgroundColor: 'white',
-                  color: '#F0AB00',
-                  fontWeight: 'bold',
-                }}>
-                  INGRESA
-                </Fab>
-              </a>
-            </Link>
-          </Box>
-          <Box
-            style={{
-              height: '',
-              width: '100%',
-              position: 'fixed',
-              bottom: 0,
-              background: '#ffffff',
-              zIndex: 1,
-            }}
-            display="flex"
-            justifyContent="space-between"
-          >
-            <img
-              src={imageLogoJA}
-              alt="LogoJA"
-              className="logos"
+const App = () => (
+  <>
+    <Box
+      style={{
+        backgroundImage: `url(${imageFondo})`,
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Box
+        style={{
+          height: '70vh',
+          width: '100%',
+        }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <img
+          src={imageCirculo}
+          alt="fondo"
+          className="circle"
+        />
+      </Box>
+      <Box
+        style={{
+          height: '30vh',
+          width: '100%',
+        }}
+        display="flex"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Link href="/objectives">
+          <a>
+            <Fab
+              aria-label="add"
+              variant="extended"
+              style={{
+                backgroundColor: 'white',
+                color: '#F0AB00',
+                fontWeight: 'bold',
+              }}
+            >
+              INGRESA
+            </Fab>
+          </a>
+        </Link>
+      </Box>
+      <Box
+        style={{
+          height: '',
+          width: '100%',
+          position: 'fixed',
+          bottom: 0,
+          background: '#ffffff',
+          zIndex: 1,
+        }}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <img
+          src={imageLogoJA}
+          alt="LogoJA"
+          className="logos"
 
-            />
-            <img
-              src={imageSAP}
-              alt="imageSAP"
-              className="logos"
-            />
-          </Box>
-        </Box>
-        <style jsx global>
-          {`
-          body {
-            margin: 0;
-            padding: 0;
-          }
+        />
+        <img
+          src={imageSAP}
+          alt="imageSAP"
+          className="logos"
+        />
+      </Box>
+    </Box>
+    <style jsx global>
+      {`
+      body {
+        margin: 0;
+        padding: 0;
+      }
+      .circle {
+        width: 80%;
+      }
+      .logos {
+        width: 40%;
+      }
+      @media screen and (min-width: 1280px) {
           .circle {
-            width: 80%;
+            width: 30%;
           }
           .logos {
-            width: 40%;
+            width: 25%;
           }
-          @media screen and (min-width: 1280px) {
-              .circle {
-                width: 30%;
-              }
-              .logos {
-                width: 25%;
-              }
-            }
-         `}
-        </style>
-      </>
-    );
-  }
-}
+        }
+      `}
+    </style>
+  </>
+);
 
 export default App;
