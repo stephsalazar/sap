993338008ef3f0ff565be7f4510a427e49dfcc37
stephSalazar <anastephany.salazarguzman@gmail.com>
2020-04-14 00:41:58 -0500

    data(programs): Add programs images

--- data/programs.js
+++ data/programs.js
@@ -65,11 +65,17 @@ export default [
     programs: [
       {
         program: 'SAP SaaS Selection Powered by Endeavor',
-        image: '',
-        text: 'Con el objetivo fue descubrir a las próximas promesas en la vertical de Software as a Service (SaaS) se seleccionaron a cinco empresas emergentes a través de una rigurosa evaluación',
-        paises: [],
+        image: 'https://user-images.githubusercontent.com/25916511/79189124-912fcb00-7de6-11ea-9184-bb22aefc31f5.jpg',
+        text: `Con el objetivo fue descubrir a las próximas promesas en la vertical de Software as a Service (SaaS) se seleccionaron a cinco empresas emergentes a través de una rigurosa evaluación con criterios de innovación, escalabilidad, impacto y tracción, considerando que todas ellas tienen más de dos años en el mercado.
+          Durante los seis meses siguientes a su selección, estas empresas recibieron un diagnóstico 360° bajo la metodología de Endeavor, mentorías con ejecutivos de SAP México y talleres especializados. El programa concluyó con un Demo Day, en el que los emprendedores presentaron ante directivos de SAP México y líderes del ecosistema los resultados obtenidos durante su aceleración.`,
+        paises: ['México'],
         ods: ['4', '8', '9', '17'],
-        more: 'con criterios de innovación, escalabilidad, impacto y tracción, considerando que todas ellas tienen más de dos años en el mercado.',
+        more: `Las startups seleccionadas se enfocan en salud, agroindustria, recursos humanos y retail:
+          Luxelare: Empresa agro-tech que ofrece a productores agrícolas y grupos agroindustriales soluciones con información valiosa que facilita la toma de decisiones.
+          BlackTrust: Empresa que, con el uso de modelos de People Analytics, ayuda a las organizaciones a mejorar sus resultados de negocio, proporcionándoles herramientas sencillas que les permitan tomar decisiones asociadas al factor humano.
+          Kuona Analytics: Solución de inteligencia artificial que ayuda a las empresas de consumo y minoristas a optimizar los precios y promociones, cartera de clientes e inventarios.
+          Cambas: Plataforma de crowdsourcing que permite a las empresas conectarse con la mayor comunidad de expertos creativos de América Latina.
+          Nubix: Sistema que permite almacenar los diagnósticos médicos en la nube, acceder a ellos desde cualquier lugar e interpretarlos.`,
       },
     ],
   },
@@ -78,17 +84,17 @@ export default [
     programs: [
       {
         program: 'Congreso EDUCA',
-        image: '',
+        image: 'https://user-images.githubusercontent.com/25916511/79189114-8d03ad80-7de6-11ea-8408-f5d14e4e320f.JPG',
         text: 'SAP México ha participado por seis años en el Congreso EDUCA, un espacio de aprendizaje y desarrollo para profesores que son parte de la RED EDUCA.',
-        paises: [],
+        paises: ['México'],
         ods: ['4', '17'],
         more: 'En su edición 2019 asistieron más de 850 profesores, quienes participaron en conferencias magistrales y talleres simultáneos enfocados en el desarrollo del pensamiento lógico-matemático, educación ambiental y liderazgo.',
       },
       {
         program: 'Más que un regalo',
-        image: '',
+        image: 'https://user-images.githubusercontent.com/25916511/79189121-8ecd7100-7de6-11ea-9f63-1d4421589b71.jpeg',
         text: 'Cada año los colaboradores de SAP México donan juguetes a niños de la RED EDUCA en el marco de Navidad y Día de Reyes. Esta actividad se lleva a cabo en Ciudad de México y Monterrey.',
-        paises: [],
+        paises: ['México'],
         ods: ['4', '17'],
         more: '',
       },
@@ -99,7 +105,7 @@ export default [
     programs: [
       {
         program: 'Diversidad e inclusión de género en la economía digital',
-        image: '',
+        image: 'https://user-images.githubusercontent.com/25916511/79189129-942abb80-7de6-11ea-9a8c-f25221976688.jpeg',
         text: 'Como parte de los compromisos de SAP México para contribuir a los ODS de Naciones Unidas, en 2019 se alió con Laboratoria para formar a la octava generación de desarrolladoras front-end y UX en la Ciudad de México.',
         paises: ['México'],
         ods: ['4', '5', '8', '17'],
@@ -112,9 +118,9 @@ export default [
     programs: [
       {
         program: 'Programa de mentoría 2019',
-        image: '',
+        image: 'https://user-images.githubusercontent.com/25916511/79189355-259a2d80-7de7-11ea-98e1-3b9ff26d1864.jpg',
         text: 'SAP México participó por primera vez en el programa anual de Mentoría para Empresarias y Emprendedoras de Vital Voices México',
-        paises: [],
+        paises: ['México'],
         ods: ['5', '8', '17'],
         more: 'con el objetivo desarrollar y fortalecer a emprendedoras a través de una relación de mentoría de nueve meses. Este proyecto benefició de manera directa a 20 mujeres propietarias de sus negocios o emprendimientos y tuvo la participación de 21 mentores y mentoras de corporativos como SAP México. Los logros obtenidos por las parejas participantes durante el proceso de mentoría se centraron en cuatro áreas: incubación, reestructuración, expansión e internacionalización y nuevas líneas de negocio.',
       },
@@ -125,9 +131,9 @@ export default [
     programs: [
       {
         program: 'Educación digital para la inclusión de mujeres en la economía',
-        image: '',
+        image: 'https://user-images.githubusercontent.com/25916511/79189108-8b39ea00-7de6-11ea-86ab-1188fa94813b.jpg',
         text: 'SAP México y WEConnect International trabajan en conjunto para acelerar la adopción digital de pymes lideradas por mujeres, ayudándolas a ser más competitivas, mejorar la calidad de los empleos que generan e innovar en sus negocios para integrarse a las cadenas de valor y así, impulsar el desarrollo económico del país.',
-        paises: [],
+        paises: ['México'],
         ods: ['4', '5', '8', '17'],
         more: 'Como parte de esta iniciativa, se lanzó el programa Herramientas digitales en la Economía de la Experiencia a través del cual se capacitaron durante 8 sesiones a 25 empresarias mexicanas en habilidades y tecnologías emergentes como Machine Learning, Internet de las Cosas, Analíticos, etc., acercándolas a la actual economía digital y su impacto en los negocios.',
       },
