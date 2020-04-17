7856749604639aafa64cf305fbfda4d3cd0318a1
stephSalazar <anastephany.salazarguzman@gmail.com>
2020-04-17 02:54:31 -0500

    Fix rectangularCard styles

--- components/rectangularCard.jsx
+++ components/rectangularCard.jsx
@@ -2,133 +2,98 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import Grid from '@material-ui/core/Grid';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 const color = {
-  '1': '#eb1c2d',
-  '2': '#D3A029',
-  '3': '#279B48',
-  '4': '#C31F33',
-  '5': '#EF402B',
-  '6': '#26AED9',
-  '7': '#FCB712',
-  '8': '#8F1838',
-  '9': '#F36D26',
-  '10': '#E11484',
-  '11': '#F99D26',
-  '12': '#CF8D2A',
-  '13': '#48773D',
-  '14': '#187DBC',
-  '15': '#3EB049',
-  '16': '#0D568B',
-  '17': '#183668',
-}
+  1: '#eb1c2d',
+  2: '#D3A029',
+  3: '#279B48',
+  4: '#C31F33',
+  5: '#EF402B',
+  6: '#26AED9',
+  7: '#FCB712',
+  8: '#8F1838',
+  9: '#F36D26',
+  10: '#E11484',
+  11: '#F99D26',
+  12: '#CF8D2A',
+  13: '#48773D',
+  14: '#187DBC',
+  15: '#3EB049',
+  16: '#0D568B',
+  17: '#183668',
+};
+
 const styles = {
-  root: {
-    boxShadow: '1px 1px 0 1px #e1e1e1',
-    marginBottom: 24,
-    '&:last-child': {
-      marginBottom: 0,
-    },
-  },
-
-  card: {
-    display: 'flex',
-    padding: 10,
-    maxWidth: 1090,
-    margin: '15px auto auto 15px',
-    // height: 150,
-  },
-
-  thumbnail: {
-    height: 100,
-    width: '100px',
-    margin: 10,
-  },
-  imagesCard: {
-    width:'100px'
-  },
-  
-  description: {
-    flexGrow: 1,
-    paddingBottom: 0,
-    paddingTop: 0,
-  },
-  objective: {
-    display:'flex',
-    flexDirection: 'row-reverse',
-    botton:0, 
-    right:0,
-  },
   span: {
     color: '#fff',
     display: 'inline-block',
-    fontSize:15,
+    fontSize: 15,
     padding: 5,
     margin: '10px 0 0 10px',
     border: '1px solid',
     borderRadius: 5,
     right: 50,
-  }
+  },
 };
 
 const RectangularCard = (props) => {
   const {
-    action,
     classes,
     thumbnail,
     title,
     subtitle,
-    objective = []
+    objective = [],
   } = props;
 
   return (
-    <Card
-      classes={{
-        root: classes.root,
-      }}
-      className={classes.card}
-    >
-      <div className={classes.thumbnail}>
-        <img className={classes.imagesCard} src={thumbnail} alt={thumbnail}/>
-      </div>
-      
-      <CardContent className={classes.description}>
-        <Typography variant="h5" component="h2" gutterBottom>
-          { title }
-        </Typography>
-        <Typography variant="body1">
-          { subtitle }
-        </Typography>
-        <Typography variant="body1" className={classes.objective}>
-          { objective.map(element => {
-            return (
-                <span style={{backgroundColor:color[element.split(" ")[1]]}} className={classes.span}>{element.split(" ")[1]}</span>
-                )
-            }) 
-          }
-        </Typography>
-      </CardContent>
+    <>
+      <Card elevation={4}>
+        <CardContent>
+          <Grid container spacing={3} alignItems="flex-end">
+            <Grid item xs={12}>
+              <Typography variant="h4" gutterBottom align="center">
+                {title}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <img src={thumbnail} className="imagePresentation" alt="dummy" />
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <Typography variant="body1" gutterBottom>
+                {subtitle}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={2}>
+              <Typography variant="body1" className={classes.objective}>
+                { objective.map(element => (
+                  <span style={{ backgroundColor: color[element.split(' ')[1]] }} className={classes.span}>{element.split(' ')[1]}</span>
+                ))
+                }
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
 
-      <CardActions disableActionSpacing>
-        { action }
-      </CardActions>
-      <style jsx>{`
-      
-      `}</style>
-    </Card>
+      <style jsx>
+        {`
+          img.imagePresentation {
+            width:100%;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
 RectangularCard.propTypes = {
-  action: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
   classes: PropTypes.shape().isRequired,
   thumbnail: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
   title: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
   subtitle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
-  objective: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 };
 
 export default withStyles(styles)(RectangularCard);
