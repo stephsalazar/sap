53ded9056ea08559e521c4919098eac1ee0fe013
stephSalazar <anastephany.salazarguzman@gmail.com>
2020-04-17 02:52:38 -0500

    Add new images for carousel

--- data/latinCodeWeek.js
+++ data/latinCodeWeek.js
@@ -4,35 +4,35 @@ const dataProyectos = {
   dataSlides: [
     {
       label: 'Más de 4 años del programa Latin Code Week en América Latina',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74395866-06c2ef00-4dd6-11ea-8932-db075c0fb6cd.jpg',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297559-21801580-7ea4-11ea-93c5-b65f868d6df0.JPG',
     },
     {
       label: 'Desarrollo de habilidades socio laborales ',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74395859-0165a480-4dd6-11ea-9afd-30b30bdb5b93.jpg',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297568-25ac3300-7ea4-11ea-989b-8c436a420444.jpg',
     },
     {
       label: 'Resolución de problemas en conjunto',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74395974-4db0e480-4dd6-11ea-80a6-26ea3fd17a9c.jpg',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297562-2349d900-7ea4-11ea-9dde-9edee3f2aff0.JPG',
     },
     {
       label: 'Emprendimiento sociales',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74396054-8d77cc00-4dd6-11ea-8025-2ba5d3e543b6.jpg',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297658-66a44780-7ea4-11ea-969f-3f4a235bc546.jpg',
     },
     {
       label: 'Integración de equipos multidisciplinarios.',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74396009-691bef80-4dd6-11ea-964b-1b2f9b175477.jpg',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297553-204ee880-7ea4-11ea-9764-fd87a9168a2a.JPG',
     },
     {
-      label: 'Integración de equipos Habilidades técnicas: desarrollo de mockup de aplicación en la plataforma SAP Build.',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74396038-818c0a00-4dd6-11ea-983f-d52c3afdd3db.jpg',
+      label: 'Habilidades técnicas: desarrollo de mockup de aplicación en la plataforma SAP Build.',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297728-9d7a5d80-7ea4-11ea-90d7-560db3dc04bd.JPG',
     },
     {
       label: 'Empresas aliadas como American Tower y Accenture que fortalecen las sesiones con la aportación de experiencia de sus voluntarios.',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74396142-c1eb8800-4dd6-11ea-888e-31d515baa19d.jpg',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297700-8c315100-7ea4-11ea-8adc-e51153cef10b.JPG',
     },
     {
       label: 'Estudiantes del Instituto Politécnico Nacional participan en Latin Code Week',
-      imgPath: 'https://user-images.githubusercontent.com/25912292/74395949-412c8c00-4dd6-11ea-9910-eaa3e0148b9b.jpg',
+      imgPath: 'https://user-images.githubusercontent.com/25916511/79297714-93585f00-7ea4-11ea-976a-ee38b5fd6b04.jpeg',
     },
   ],
 
@@ -44,20 +44,20 @@ const dataProyectos = {
       objective: ['Objetivo 8 Trabajo decente y crecimiento económico'],
     },
     {
-      title: 'Uni App',
-      subtitulo: 'Aplicación que brinda opciones de educación universitaria con becas, programas e intercambios. Reto: Mejorar la Calidad de la Educación en México',
-      thumbnail: 'https://user-images.githubusercontent.com/25912292/74395355-bf882e80-4dd4-11ea-8c82-c97c42f7dad5.JPG',
-      objective: ['Objetivo 4 Educación de Calidad'],
-    },
-    {
       title: 'Girly Protect',
       subtitulo: 'Pulsera para la seguridad de mujeres. Reto: Ciudades inteligentes ',
       thumbnail: 'https://user-images.githubusercontent.com/25912292/74394974-b185de00-4dd3-11ea-9178-32123f453aec.jpg',
       objective: ['Objetivo 3 Salud y Bienestar ', 'Objetivo 9 Industria, Innovación e Infraestructura'],
     },
     {
+      title: 'Uni App',
+      subtitulo: 'Aplicación que brinda opciones de educación universitaria con becas, programas e intercambios. Reto: Mejorar la Calidad de la Educación en México',
+      thumbnail: 'https://user-images.githubusercontent.com/25912292/74395355-bf882e80-4dd4-11ea-8c82-c97c42f7dad5.JPG',
+      objective: ['Objetivo 4 Educación de Calidad'],
+    },
+    {
       title: 'Security way',
-      subtitulo: 'transporte seguro y accesible para traslado de estudiantes. Reto: Deserción escolar',
+      subtitulo: 'Transporte seguro y accesible para traslado de estudiantes. Reto: Deserción escolar',
       thumbnail: 'https://user-images.githubusercontent.com/25912292/74407837-734ee580-4df8-11ea-9ea2-2b63b1cb8ea3.JPG',
       objective: ['Objetivo 11 Ciudades y Comunidades Sostenibles', 'Objetivo 4 Educación de Calidad'],
     },
